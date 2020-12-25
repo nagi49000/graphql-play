c9cdde3c6ca4e5fcc093ba02f57693e23d7f69c4
nagi49000 <nagi49000@gmail.com>
2020-12-25 23:32:37 +0000

    rename and add of ariadne app

--- graphene_app/graphene_app/simple_graphene_app.py
+++ graphene_app/graphene_app/simple_graphene_app.py
@@ -1,12 +1,20 @@
-from graphene import ObjectType, String, Schema
+from graphene import ObjectType, String, Int, List, NonNull, Schema
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
+
+
+class Person(ObjectType):
+    firstName = String()
+    lastName = String()
+    age = Int()
+    fullName = String()
 
 
 class Query(ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `name`
     hello = String(name=String(default_value="stranger"))
     goodbye = String()
+    people = NonNull(List(NonNull(Person)))
 
     # our Resolver method takes the GraphQL context (root, info) as well as
     # Argument (name) for the Field and returns data for the query Response
@@ -15,6 +23,10 @@ class Query(ObjectType):
 
     def resolve_goodbye(root, info):
         return 'See ya!'
+
+    def resolve_people(root, info):
+        return [Person(firstName=x, lastName=y, age=z, fullName=x+' '+y)
+                for x, y, z in [["John", "Doe", 21], ["Bob", "Boberson", 24]]]
 
 
 def get_app():
