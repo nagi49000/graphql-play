d9f1ae06b7aab2c2e3a222eff0dc91047956ec9a
nagi49000 <nagi49000@gmail.com>
2020-12-26 20:06:22 +0000

    first cut plumbing into sqlite

--- graphene_app/graphene_app/simple_graphene_app.py
+++ graphene_app/graphene_app/simple_graphene_app.py
@@ -1,23 +1,57 @@
-from graphene import ObjectType, String, Int, List, NonNull, Schema
+import os
+import sqlalchemy as sa
+from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
+from sqlalchemy.orm import scoped_session, sessionmaker
+import graphene as gp
+from graphene_sqlalchemy import SQLAlchemyObjectType
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from starlette.routing import Route
 from starlette.applications import Starlette
 from starlette.graphql import GraphQLApp
 
 
-class Person(ObjectType):
-    firstName = String(required=True)
-    lastName = String(required=True)
-    age = Int(required=True)
-    fullName = String()
+# Database specific stuff
 
 
-class ToySimples(ObjectType):
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sql_db = 'sqlite:///' + os.path.join(this_dir, '..', '..', 'data', 'chinook.db')
+sql_engine = sa.engine.create_engine(sql_db)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=sql_engine))
+Base = declarative_base(cls=DeferredReflection)
+Base.query = db_session.query_property()
+
+
+class CustomerModel(Base):
+    __tablename__ = 'customers'
+    CustomerId = sa.Column(sa.Integer, primary_key=True)
+
+
+Base.prepare(sql_engine)
+
+# GraphQL schema specific stuff
+
+
+class Customer(SQLAlchemyObjectType):
+    class Meta:
+        model = CustomerModel
+        interfaces = (gp.relay.Node, )
+
+
+class Person(gp.ObjectType):
+    firstName = gp.String(required=True)
+    lastName = gp.String(required=True)
+    age = gp.Int(required=True)
+    fullName = gp.String()
+
+
+class ToySimples(gp.ObjectType):
     # this defines a Field `hello` in our Schema with a single Argument `name`
-    hello = String(name=String(default_value="stranger"))
-    goodbye = String()
-    people = NonNull(List(NonNull(Person)),
-                     max_age=Int(default_value=60))
+    hello = gp.String(name=gp.String(default_value="stranger"))
+    goodbye = gp.String()
+    people = gp.NonNull(gp.List(gp.NonNull(Person)),
+                        max_age=gp.Int(default_value=60))
 
     # our Resolver method takes the GraphQL context (root, info) as well as
     # Argument (name) for the Field and returns data for the query Response
@@ -34,17 +68,26 @@ class ToySimples(ObjectType):
                 if z <= max_age]
 
 
-class Chinook(ObjectType):
-    version = String()
+class Chinook(gp.ObjectType):
+    version = gp.String()
+    node = gp.relay.Node.Field()
+    customer = gp.List(Customer)
 
     def resolve_version(root, info):
         return "0.0.1"
 
+    def resolve_customer(self, info):
+        print(info)
+        query = Customer.get_query(info)
+        print(query)
+        return query.all()
+
 
 def get_app():
-    schema = Schema(query=ToySimples)
     routes = [Route("/simple-graphene/api/v1/",
-                    GraphQLApp(schema=schema,
+                    GraphQLApp(schema=gp.Schema(query=ToySimples))),
+              Route("/chinook-graphene/api/v1/",
+                    GraphQLApp(schema=gp.Schema(query=Chinook, types=[Customer]),
                                executor_class=AsyncioExecutor))]
     return Starlette(routes=routes)
 
