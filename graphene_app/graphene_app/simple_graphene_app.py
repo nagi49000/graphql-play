dea725d976e43079df5392b9f1ce6d95cba22e4a
nagi49000 <nagi49000@gmail.com>
2020-12-26 23:05:18 +0000

    set up files for unit testing

--- graphene_app/graphene_app/simple_graphene_app.py
+++ graphene_app/graphene_app/simple_graphene_app.py
@@ -3,40 +3,13 @@ import sqlalchemy as sa
 from sqlalchemy.ext.declarative import declarative_base, DeferredReflection
 from sqlalchemy.orm import scoped_session, sessionmaker
 import graphene as gp
-from graphene_sqlalchemy import SQLAlchemyObjectType
+from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
 from graphql.execution.executors.asyncio import AsyncioExecutor
 from starlette.routing import Route
 from starlette.applications import Starlette
 from starlette.graphql import GraphQLApp
 
-
-# Database specific stuff
-
-
-this_dir = os.path.dirname(os.path.abspath(__file__))
-sql_db = 'sqlite:///' + os.path.join(this_dir, '..', '..', 'data', 'chinook.db')
-sql_engine = sa.engine.create_engine(sql_db)
-db_session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=sql_engine))
-Base = declarative_base(cls=DeferredReflection)
-Base.query = db_session.query_property()
-
-
-class CustomerModel(Base):
-    __tablename__ = 'customers'
-    CustomerId = sa.Column(sa.Integer, primary_key=True)
-
-
-Base.prepare(sql_engine)
-
-# GraphQL schema specific stuff
-
-
-class Customer(SQLAlchemyObjectType):
-    class Meta:
-        model = CustomerModel
-        interfaces = (gp.relay.Node, )
+# simple no database example
 
 
 class Person(gp.ObjectType):
@@ -68,19 +41,157 @@ class ToySimples(gp.ObjectType):
                 if z <= max_age]
 
 
+# Database example
+# Database specific stuff
+
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sql_db = 'sqlite:///' + os.path.join(this_dir, '..', '..', 'data', 'chinook.db')
+sql_engine = sa.engine.create_engine(sql_db)
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                         autoflush=False,
+                                         bind=sql_engine))
+Base = declarative_base(cls=DeferredReflection)
+Base.query = db_session.query_property()
+
+
+# Build up the Data models, using sqlalchemy.ext.declarative to
+# pull in the models from the SQL DB Metadata
+
+class CustomerModel(Base):
+    __tablename__ = 'customers'
+    # CustomerId = sa.Column(sa.Integer, primary_key=True)
+
+
+class ArtistModel(Base):
+    __tablename__ = 'artists'
+
+
+class EmployeeModel(Base):
+    __tablename__ = 'employees'
+
+
+class GenreModel(Base):
+    __tablename__ = 'genres'
+
+
+class MediaTypeModel(Base):
+    __tablename__ = 'media_types'
+
+
+class PlaylistModel(Base):
+    __tablename__ = 'playlists'
+
+
+class AlbumModel(Base):
+    __tablename__ = 'albums'
+
+
+class InvoiceModel(Base):
+    __tablename__ = 'invoices'
+
+
+class TrackModel(Base):
+    __tablename__ = 'tracks'
+
+
+class InvoiceItemModel(Base):
+    __tablename__ = 'invoice_items'
+
+
+class PlaylistTrackModel(Base):
+    __tablename__ = 'playlist_track'
+
+
+Base.prepare(sql_engine)
+
+# GraphQL schema specific stuff
+# Build up the Graphene objects that will make up the Graph QL schema
+
+
+class Customer(SQLAlchemyObjectType):
+    class Meta:
+        model = CustomerModel
+        interfaces = (gp.relay.Node, )
+
+
+class Artist(SQLAlchemyObjectType):
+    class Meta:
+        model = ArtistModel
+        interfaces = (gp.relay.Node, )
+
+
+class Employee(SQLAlchemyObjectType):
+    class Meta:
+        model = EmployeeModel
+        interfaces = (gp.relay.Node, )
+
+
+class Genre(SQLAlchemyObjectType):
+    class Meta:
+        model = GenreModel
+        interfaces = (gp.relay.Node, )
+
+
+class MediaType(SQLAlchemyObjectType):
+    class Meta:
+        model = MediaTypeModel
+        interfaces = (gp.relay.Node, )
+
+
+class Playlist(SQLAlchemyObjectType):
+    class Meta:
+        model = PlaylistModel
+        interfaces = (gp.relay.Node, )
+
+
+class Album(SQLAlchemyObjectType):
+    class Meta:
+        model = AlbumModel
+        interfaces = (gp.relay.Node, )
+
+
+class Invoice(SQLAlchemyObjectType):
+    class Meta:
+        model = InvoiceModel
+        interfaces = (gp.relay.Node, )
+
+
+class Track(SQLAlchemyObjectType):
+    class Meta:
+        model = TrackModel
+        interfaces = (gp.relay.Node, )
+
+
+class InvoiceItem(SQLAlchemyObjectType):
+    class Meta:
+        model = InvoiceItemModel
+        interfaces = (gp.relay.Node, )
+
+
+class PlaylistTrack(SQLAlchemyObjectType):
+    class Meta:
+        model = PlaylistTrackModel
+        interfaces = (gp.relay.Node, )
+
+
 class Chinook(gp.ObjectType):
     version = gp.String()
+    # use SQLAlchemyConnectionField to build up queries for
+    # Graphene objects above. Standard queries pull ALL rows
     node = gp.relay.Node.Field()
-    customer = gp.List(Customer)
+    all_customers = SQLAlchemyConnectionField(Customer.connection)
+    all_artists = SQLAlchemyConnectionField(Artist.connection)
+    all_genres = SQLAlchemyConnectionField(Genre.connection)
+    all_media_types = SQLAlchemyConnectionField(MediaType.connection)
+    all_playlists = SQLAlchemyConnectionField(Playlist.connection)
+    all_albums = SQLAlchemyConnectionField(Album.connection)
+    all_invoices = SQLAlchemyConnectionField(Invoice.connection)
+    all_tracks = SQLAlchemyConnectionField(Track.connection)
+    all_invoice_items = SQLAlchemyConnectionField(InvoiceItem.connection)
+    all_playlist_tracks = SQLAlchemyConnectionField(PlaylistTrack.connection)
 
     def resolve_version(root, info):
         return "0.0.1"
-
-    def resolve_customer(self, info):
-        print(info)
-        query = Customer.get_query(info)
-        print(query)
-        return query.all()
 
 
 def get_app():
---
+++ graphene_app/tests/test_simple_graphene_app.py
@@ -0,0 +1,5 @@
+from ..graphene_app.simple_graphene_app import get_app
+
+
+def test_simple_graphene():
+    pass
